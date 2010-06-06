3139450dd80b4909119ee9421866cdb6a8cc38bd
Chris Done <chrisdone@Host.homenet.telecomitalia.it>
2010-06-06 18:43:16 +0200

    fixed css gzip compressor content type

--- gzip.php
+++ gzip.php
@@ -1,6 +1,7 @@
 <?php
 ob_start("ob_gzhandler");
 if (preg_match('/.css/',$_GET['file'])) {
+  header("Content-Type: text/css");
   $_GET['file'] = preg_replace('/[.]+/','.',$_GET['file']);
   readfile('.'.$_GET['file']);
 }
