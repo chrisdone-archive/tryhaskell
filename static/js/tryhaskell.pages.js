2209f378e4e894146759897ce890ef132d6b6679
Chris Done <chrisdone@gmail.com>
2014-01-11 19:22:13 +0100

    Update hackage link

--- static/js/tryhaskell.pages.js
+++ static/js/tryhaskell.pages.js
@@ -37,7 +37,7 @@ tryhaskell.pages.list =
          '&quot;hello&quot;&quot; into the console." style="cursor: pointer;">reverse ' +
          '"hello"</code> or <code title="Click me to insert &quot;foldr (:) [] [1,2,3]&quot; into the console." style="cursor: pointer;">foldr (:) [] [1,2,3]</code> or <code title="Click me to insert." style="cursor: pointer;">do line <- getLine; putStrLn line</code> or <code>readFile "/welcome"</code>' +
          '</p>' +
-         '<p><a href="https://hackage.haskell.org/package/pure-io-0.1.1/docs/PureIO.html#g:2">These</a> IO actions are supported in this app.</p>' +
+         '<p><a href="https://hackage.haskell.org/package/pure-io-0.2.0/docs/PureIO.html#g:2">These</a> IO actions are supported in this app.</p>' +
          '<p><strong>Other cool learning places</strong></pa>' +
          '<p class="clearfix">' +
          'You can also run real Haskell code—file I/O, ' +
